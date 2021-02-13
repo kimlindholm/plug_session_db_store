7e0db35ebbdbb6845f4744f4991a2cd458e526e9
Kim Lindholm <misc@dynevision.com>
2021-02-13 14:41:25 +0700

    Optional signing and encryption of sessions

--- lib/plug_session_db_store_web/db_session_store.ex
+++ lib/plug_session_db_store_web/db_session_store.ex
@@ -2,10 +2,75 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
   @moduledoc """
   Stores the session in the database.
 
+  This module is based on code published here
+  https://github.com/elixir-plug/plug/blob/2361bd3ca8f4c3ecd44c1ff33df6c184f7cbf512/lib/plug/session/cookie.ex
+  as part of the Plug project, as of Feb 7 2021. It is used in
+  accordance with the project's license:
+
+  > Copyright (c) 2013 Plataformatec.
+  >
+  >   Licensed under the Apache License, Version 2.0 (the "License");
+  >   you may not use this file except in compliance with the License.
+  >   You may obtain a copy of the License at
+  >
+  >       http://www.apache.org/licenses/LICENSE-2.0
+  >
+  >   Unless required by applicable law or agreed to in writing, software
+  >   distributed under the License is distributed on an "AS IS" BASIS,
+  >   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  >   See the License for the specific language governing permissions and
+  >   limitations under the License.
+
+  This store is based on `Plug.Crypto.MessageVerifier` and
+  `Plug.Crypto.MessageEncryptor` which encrypts and signs session data
+  to ensure they can't be read nor tampered with.
+
+  Since this store uses crypto features, it requires you to set the
+  `:secret_key_base` field in your connection. This can be easily
+  achieved with a plug:
+
+      plug :put_secret_key_base
+
+      def put_secret_key_base(conn, _) do
+        put_in conn.secret_key_base, "-- LONG STRING WITH AT LEAST 64 BYTES --"
+      end
+
+  ## Miscellaneous
+
   In order to clean up expired sessions, a periodic task needs to be
   created.
 
   ## Options
+
+    * `:secret_key_base` - the secret key base to build the
+      signing/encryption on top of. If one is given on initialization,
+      the database store can precompute all relevant values at
+      compilation time. Otherwise, the value is taken from
+      `conn.secret_key_base` and cached.
+
+    * `:encryption_salt` - a salt used with `conn.secret_key_base` to
+      generate a key for encrypting/decrypting data, can be either a
+      binary or an MFA returning a binary;
+
+    * `:signing_salt` - a salt used with `conn.secret_key_base` to
+      generate a key for signing/verifying data, can be either a binary
+      or an MFA returning a binary;
+
+    * `:key_iterations` - option passed to `Plug.Crypto.KeyGenerator`
+      when generating the encryption and signing keys. Defaults to 1000;
+
+    * `:key_length` - option passed to `Plug.Crypto.KeyGenerator`
+      when generating the encryption and signing keys. Defaults to 32;
+
+    * `:key_digest` - option passed to `Plug.Crypto.KeyGenerator`
+      when generating the encryption and signing keys. Defaults to `:sha256`;
+
+    * `:serializer` - serializer module that defines `encode/1` and
+      `decode/1` returning an `{:ok, value}` tuple. Defaults to
+      `:external_term_format`.
+
+    * `:log` - Log level to use when the data cannot be decoded.
+      Defaults to `:debug`, can be set to false to disable it.
 
     * `:store_max_age` - Session expiry in seconds, defaults to 1 hour
 
@@ -16,6 +81,10 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
       plug(Plug.Session,
         store: MyAppWeb.DbSessionStore,
         key: "_my_app_key",
+        encryption_salt: "encryption salt",
+        signing_salt: "signing salt",
+        key_length: 64,
+        log: :debug,
         max_age: max_age,
         store_max_age: max_age
       )
@@ -24,14 +93,37 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
 
   @behaviour Plug.Session.Store
 
+  require Logger
+
+  alias Plug.Crypto.KeyGenerator
+  alias Plug.Crypto.MessageEncryptor
+  alias Plug.Crypto.MessageVerifier
   alias PlugSessionDbStore.Sessions
   alias PlugSessionDbStore.Sessions.Session
 
   @impl true
   def init(opts \\ []) do
+    encryption_salt = opts[:encryption_salt]
+    signing_salt = check_signing_salt(opts)
+
+    iterations = Keyword.get(opts, :key_iterations, 1000)
+    length = Keyword.get(opts, :key_length, 32)
+    digest = Keyword.get(opts, :key_digest, :sha256)
+    log = Keyword.get(opts, :log, :debug)
+    secret_key_base = Keyword.get(opts, :secret_key_base)
+    key_opts = [iterations: iterations, length: length, digest: digest, cache: Plug.Keys]
     max_age = Keyword.get(opts, :store_max_age, 3600)
 
-    %{max_age: max_age}
+    serializer = check_serializer(opts[:serializer] || :external_term_format)
+
+    %{
+      encryption_salt: prederive(secret_key_base, encryption_salt, key_opts),
+      signing_salt: prederive(secret_key_base, signing_salt, key_opts),
+      key_opts: key_opts,
+      serializer: serializer,
+      log: log,
+      max_age: max_age
+    }
   end
 
   @impl true
@@ -42,22 +134,22 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
   end
 
   @impl true
-  def get(_conn, cookie, opts) do
+  def get(conn, cookie, opts) do
     session = Sessions.get_by_session_cookie(cookie)
-    get_for_session(cookie, session, opts)
+    get_for_session(conn, cookie, session, opts)
   end
 
   @impl true
-  def put(_conn, cookie, term, opts)
+  def put(conn, cookie, term, opts)
       when cookie == ""
       when is_nil(cookie) do
-    put_new(term, opts)
+    put_new(conn, term, opts)
   end
 
   @impl true
-  def put(_conn, cookie, term, opts) do
+  def put(conn, cookie, term, opts) do
     session = Sessions.get_by_session_cookie(cookie)
-    put_for_session(cookie, session, term, opts)
+    put_for_session(conn, cookie, session, term, opts)
   end
 
   @impl true
@@ -73,15 +165,16 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
     :ok
   end
 
-  defp get_for_session(_cookie, nil, _opts), do: {nil, %{}}
+  defp get_for_session(_conn, _cookie, nil, _opts), do: {nil, %{}}
 
   defp get_for_session(
+         conn,
          cookie,
          %Session{valid_to: valid_to, data: data} = session,
-         %{max_age: max_age}
+         %{max_age: max_age} = opts
        ) do
     if DateTime.compare(now(), valid_to) == :lt do
-      term = :erlang.binary_to_term(data)
+      {_, term} = data_to_term(conn, data, opts)
       valid_to = seconds_from(now(), max_age)
 
       Sessions.update_session!(session, %{valid_to: valid_to})
@@ -92,34 +185,36 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
     end
   end
 
-  defp put_new(term, opts) do
-    create_session!(term, opts)
+  defp put_new(conn, term, opts) do
+    create_session!(conn, term, opts)
   end
 
-  defp put_for_session(cookie, nil, term, opts) do
-    create_session!(cookie, term, opts)
+  defp put_for_session(conn, cookie, nil, term, opts) do
+    create_session!(conn, cookie, term, opts)
   end
 
   defp put_for_session(
+         conn,
          cookie,
          %Session{valid_to: valid_to} = session,
          term,
-         %{max_age: max_age}
+         opts
        ) do
     if DateTime.compare(now(), valid_to) == :lt do
-      data = :erlang.term_to_binary(term)
-      valid_to = seconds_from(now(), max_age)
+      data = term_to_data(conn, term, opts)
+      valid_to = seconds_from(now(), opts.max_age)
 
       Sessions.update_session!(session, %{data: data, valid_to: valid_to})
+
       cookie
     else
       nil
     end
   end
 
-  defp create_session!(cookie \\ nil, term, %{max_age: max_age}) do
+  defp create_session!(conn, cookie \\ nil, term, %{max_age: max_age} = opts) do
     cookie = cookie || :crypto.strong_rand_bytes(96) |> Base.encode64()
-    data = :erlang.term_to_binary(term)
+    data = term_to_data(conn, term, opts)
     valid_from = now()
     valid_to = seconds_from(valid_from, max_age)
 
@@ -132,6 +227,144 @@ defmodule PlugSessionDbStoreWeb.DbSessionStore do
 
     cookie
   end
+
+  defp data_to_term(conn, data, %{
+         key_opts: key_opts,
+         encryption_salt: encryption_salt,
+         signing_salt: signing_salt,
+         log: log,
+         serializer: serializer
+       }) do
+    case encryption_salt do
+      nil ->
+        MessageVerifier.verify(
+          data,
+          derive(conn.secret_key_base, signing_salt, key_opts)
+        )
+
+      encryption_salt ->
+        MessageEncryptor.decrypt(
+          data,
+          derive(conn.secret_key_base, encryption_salt, key_opts),
+          derive(conn.secret_key_base, signing_salt, key_opts)
+        )
+    end
+    |> decode(serializer, log)
+  end
+
+  defp term_to_data(conn, term, %{
+         serializer: serializer,
+         key_opts: key_opts,
+         encryption_salt: encryption_salt,
+         signing_salt: signing_salt
+       }) do
+    binary = encode(term, serializer)
+
+    case encryption_salt do
+      nil ->
+        MessageVerifier.sign(
+          binary,
+          derive(conn.secret_key_base, signing_salt, key_opts)
+        )
+
+      encryption_salt ->
+        MessageEncryptor.encrypt(
+          binary,
+          derive(conn.secret_key_base, encryption_salt, key_opts),
+          derive(conn.secret_key_base, signing_salt, key_opts)
+        )
+    end
+  end
+
+  defp encode(term, :external_term_format) do
+    :erlang.term_to_binary(term)
+  end
+
+  defp encode(term, serializer) do
+    {:ok, binary} = serializer.encode(term)
+    binary
+  end
+
+  defp decode({:ok, binary}, :external_term_format, log) do
+    {:term,
+     try do
+       Plug.Crypto.non_executable_binary_to_term(binary)
+     rescue
+       e ->
+         Logger.log(
+           log,
+           "Plug.Session could not decode incoming session data. Reason: " <>
+             Exception.message(e)
+         )
+
+         %{}
+     end}
+  end
+
+  defp decode({:ok, binary}, serializer, _log) do
+    case serializer.decode(binary) do
+      {:ok, term} -> {:custom, term}
+      _ -> {:custom, %{}}
+    end
+  end
+
+  defp decode(:error, _serializer, false) do
+    {nil, %{}}
+  end
+
+  defp decode(:error, _serializer, log) do
+    Logger.log(
+      log,
+      "Plug.Session could not verify incoming session data. " <>
+        "This may happen when the session settings change or a stale cookie is sent."
+    )
+
+    {nil, %{}}
+  end
+
+  defp prederive(secret_key_base, value, key_opts)
+       when is_binary(secret_key_base) and is_binary(value) do
+    {:prederived, derive(secret_key_base, value, Keyword.delete(key_opts, :cache))}
+  end
+
+  defp prederive(_secret_key_base, value, _key_opts) do
+    value
+  end
+
+  defp derive(_secret_key_base, {:prederived, value}, _key_opts) do
+    value
+  end
+
+  defp derive(secret_key_base, {module, function, args}, key_opts) do
+    derive(secret_key_base, apply(module, function, args), key_opts)
+  end
+
+  defp derive(secret_key_base, key, key_opts) do
+    secret_key_base
+    |> validate_secret_key_base()
+    |> KeyGenerator.generate(key, key_opts)
+  end
+
+  defp validate_secret_key_base(nil),
+    do: raise(ArgumentError, "database store expects conn.secret_key_base to be set")
+
+  defp validate_secret_key_base(secret_key_base) when byte_size(secret_key_base) < 64,
+    do:
+      raise(ArgumentError, "database store expects conn.secret_key_base to be at least 64 bytes")
+
+  defp validate_secret_key_base(secret_key_base), do: secret_key_base
+
+  defp check_signing_salt(opts) do
+    case opts[:signing_salt] do
+      nil -> raise ArgumentError, "database store expects :signing_salt as option"
+      salt -> salt
+    end
+  end
+
+  defp check_serializer(serializer) when is_atom(serializer), do: serializer
+
+  defp check_serializer(_),
+    do: raise(ArgumentError, "database store expects :serializer option to be a module")
 
   defp now do
     DateTime.utc_now()
--- test/plug_session_db_store_web/db_session_store_test.exs
+++ test/plug_session_db_store_web/db_session_store_test.exs
@@ -1,96 +1,349 @@
 defmodule PlugSessionDbStoreWeb.DbSessionStoreTest do
   use PlugSessionDbStoreWeb.ConnCase, async: true
+  use Plug.Test
 
   alias PlugSessionDbStore.Repo
   alias PlugSessionDbStore.Sessions.Session
   alias PlugSessionDbStoreWeb.DbSessionStore, as: DbStore
 
+  @default_opts [
+    store: DbStore,
+    key: "foobar",
+    encryption_salt: "encryption salt",
+    signing_salt: "signing salt",
+    log: false
+  ]
+
+  @secret String.duplicate("abcdef0123456789", 8)
+  @signing_opts Plug.Session.init(Keyword.put(@default_opts, :encrypt, false))
+  @encrypted_opts Plug.Session.init(@default_opts)
+  @prederived_opts Plug.Session.init([secret_key_base: @secret] ++ @default_opts)
+
+  defmodule CustomSerializer do
+    def encode(%{"foo" => "bar"}), do: {:ok, "encoded session"}
+    def encode(%{"foo" => "baz"}), do: {:ok, "another encoded session"}
+    def encode(%{}), do: {:ok, ""}
+    def encode(_), do: :error
+
+    def decode("encoded session"), do: {:ok, %{"foo" => "bar"}}
+    def decode("another encoded session"), do: {:ok, %{"foo" => "baz"}}
+    def decode(nil), do: {:ok, nil}
+    def decode(_), do: :error
+  end
+
+  opts = Keyword.put(@default_opts, :serializer, CustomSerializer)
+  @custom_serializer_opts Plug.Session.init(opts)
+
+  @conn %{secret_key_base: @secret}
+  @opts @encrypted_opts.store_config
+
   describe "init/1" do
-    test "when opts are not specified, uses default values" do
-      opts = DbStore.init()
-      assert opts[:max_age] == 3600
+    test "requires signing_salt option to be defined" do
+      assert_raise ArgumentError, ~r/expects :signing_salt as option/, fn ->
+        Plug.Session.init(Keyword.delete(@default_opts, :signing_salt))
+      end
     end
 
-    test "uses specified opts" do
-      opts = DbStore.init(store_max_age: 7200)
-      assert opts[:max_age] == 7200
+    test "requires the secret to be at least 64 bytes" do
+      assert_raise ArgumentError, ~r/to be at least 64 bytes/, fn ->
+        build_conn(:get, "/")
+        |> sign_conn("abcdef")
+        |> put_session("foo", "bar")
+        |> send_resp(200, "OK")
+      end
+    end
+
+    test "default key generator opts" do
+      key_generator_opts = DbStore.init(@default_opts).key_opts
+      assert key_generator_opts[:iterations] == 1000
+      assert key_generator_opts[:length] == 32
+      assert key_generator_opts[:digest] == :sha256
+    end
+
+    test "prederives keys if secret_key_base is available" do
+      assert %{encryption_salt: {:prederived, _}, signing_salt: {:prederived, _}} =
+               DbStore.init([secret_key_base: @secret] ++ @default_opts)
+    end
+
+    test "uses specified key generator opts" do
+      opts =
+        @default_opts
+        |> Keyword.put(:key_iterations, 2000)
+        |> Keyword.put(:key_length, 64)
+        |> Keyword.put(:key_digest, :sha)
+
+      key_generator_opts = DbStore.init(opts).key_opts
+      assert key_generator_opts[:iterations] == 2000
+      assert key_generator_opts[:length] == 64
+      assert key_generator_opts[:digest] == :sha
+    end
+
+    test "requires serializer option to be an atom" do
+      assert_raise ArgumentError, ~r/expects :serializer option to be a module/, fn ->
+        Plug.Session.init(Keyword.put(@default_opts, :serializer, "CustomSerializer"))
+      end
+    end
+
+    test "uses :external_term_format cookie serializer by default" do
+      assert Plug.Session.init(@default_opts).store_config.serializer == :external_term_format
+    end
+
+    test "uses custom cookie serializer" do
+      assert @custom_serializer_opts.store_config.serializer == CustomSerializer
+    end
+
+    test "uses MFAs for salts" do
+      opts = [
+        store: DbStore,
+        key: "foobar",
+        encryption_salt: {__MODULE__, :returns_arg, ["encryption salt"]},
+        signing_salt: {__MODULE__, :returns_arg, ["signing salt"]}
+      ]
+
+      plug = Plug.Session.init(opts)
+      assert apply_mfa(plug.store_config.encryption_salt) == "encryption salt"
+      assert apply_mfa(plug.store_config.signing_salt) == "signing salt"
+    end
+
+    test "when :max_age is not specified, uses a default value" do
+      assert Plug.Session.init(@default_opts).store_config.max_age == 3600
+    end
+
+    test "uses specified :max_age" do
+      opts = [store_max_age: 7200] ++ @default_opts
+      assert Plug.Session.init(opts).store_config.max_age == 7200
     end
   end
 
   test "put and get session" do
-    opts = DbStore.init()
     refute Repo.get_by(Session, session_cookie: "foo")
 
-    assert "foo" = DbStore.put(%{}, "foo", %{foo: :bar}, opts)
-    assert "bar" = DbStore.put(%{}, "bar", %{bar: :foo}, opts)
+    assert "foo" = DbStore.put(@conn, "foo", %{foo: :bar}, @opts)
+    assert "bar" = DbStore.put(@conn, "bar", %{bar: :foo}, @opts)
 
     assert Repo.get_by(Session, session_cookie: "foo")
 
-    assert {"foo", %{foo: :bar}} = DbStore.get(%{}, "foo", opts)
-    assert {"bar", %{bar: :foo}} = DbStore.get(%{}, "bar", opts)
+    assert {"foo", %{foo: :bar}} = DbStore.get(@conn, "foo", @opts)
+    assert {"bar", %{bar: :foo}} = DbStore.get(@conn, "bar", @opts)
 
-    assert {nil, %{}} = DbStore.get(%{}, "unknown", opts)
+    assert {nil, %{}} = DbStore.get(@conn, "unknown", @opts)
   end
 
   test "ignores expired sessions" do
-    opts = DbStore.init(store_max_age: 1800)
+    opts = %{@opts | max_age: 1800}
 
-    assert "foo" = DbStore.put(%{}, "foo", %{foo: :bar}, opts)
+    assert "foo" = DbStore.put(@conn, "foo", %{foo: :bar}, opts)
     assert Repo.get_by(Session, session_cookie: "foo")
-    assert {"foo", %{foo: :bar}} = DbStore.get(%{}, "foo", opts)
+    assert {"foo", %{foo: :bar}} = DbStore.get(@conn, "foo", opts)
 
-    assert "foo" = DbStore.put(%{}, "foo", %{bar: :foo}, opts)
-    assert {"foo", %{bar: :foo}} = DbStore.get(%{}, "foo", opts)
+    assert "foo" = DbStore.put(@conn, "foo", %{bar: :foo}, opts)
+    assert {"foo", %{bar: :foo}} = DbStore.get(@conn, "foo", opts)
 
-    opts = DbStore.init(store_max_age: -10)
+    opts = %{@opts | max_age: -10}
 
-    assert "bar" = DbStore.put(%{}, "bar", %{bar: :foo}, opts)
+    assert "bar" = DbStore.put(@conn, "bar", %{bar: :foo}, opts)
     assert Repo.get_by(Session, session_cookie: "bar")
-    assert {nil, %{}} = DbStore.get(%{}, "bar", opts)
+    assert {nil, %{}} = DbStore.get(@conn, "bar", opts)
 
-    assert nil == DbStore.put(%{}, "bar", %{foo: :bar}, opts)
-    assert {nil, %{}} = DbStore.get(%{}, "bar", opts)
+    assert nil == DbStore.put(@conn, "bar", %{foo: :bar}, opts)
+    assert {nil, %{}} = DbStore.get(@conn, "bar", opts)
   end
 
   test "updates session expiry on reads and writes" do
-    opts = DbStore.init()
-
-    DbStore.put(%{}, "foo", %{foo: :bar}, opts)
+    DbStore.put(@conn, "foo", %{foo: :bar}, @opts)
     assert session = Repo.get_by(Session, session_cookie: "foo")
     assert expiry_1 = session.valid_to
 
-    DbStore.get(%{}, "foo", opts)
+    DbStore.get(@conn, "foo", @opts)
     assert session = Repo.get_by(Session, session_cookie: "foo")
     assert expiry_2 = session.valid_to
     assert DateTime.compare(expiry_1, expiry_2) == :lt
 
-    DbStore.put(%{}, "foo", %{bar: :foo}, opts)
+    DbStore.put(@conn, "foo", %{bar: :foo}, @opts)
     assert session = Repo.get_by(Session, session_cookie: "foo")
     assert DateTime.compare(expiry_2, session.valid_to) == :lt
   end
 
   test "delete session" do
-    opts = DbStore.init()
-
-    DbStore.put(%{}, "foo", %{foo: :bar}, opts)
-    DbStore.put(%{}, "bar", %{bar: :foo}, opts)
+    DbStore.put(@conn, "foo", %{foo: :bar}, @opts)
+    DbStore.put(@conn, "bar", %{bar: :foo}, @opts)
     assert Repo.get_by(Session, session_cookie: "foo")
 
-    DbStore.delete(%{}, "foo", opts)
+    DbStore.delete(@conn, "foo", @opts)
 
-    assert {nil, %{}} = DbStore.get(%{}, "foo", opts)
-    assert {"bar", %{bar: :foo}} = DbStore.get(%{}, "bar", opts)
+    assert {nil, %{}} = DbStore.get(@conn, "foo", @opts)
+    assert {"bar", %{bar: :foo}} = DbStore.get(@conn, "bar", @opts)
     refute Repo.get_by(Session, session_cookie: "foo")
   end
 
   test "generate new sid" do
-    opts = DbStore.init()
-    sid = DbStore.put(%{}, nil, %{}, opts)
+    sid = DbStore.put(@conn, nil, %{}, @opts)
     assert byte_size(sid) == 128
   end
 
   test "invalidate sid if unknown" do
-    opts = DbStore.init()
-    assert {nil, %{}} = DbStore.get(%{}, "unknown_sid", opts)
+    assert {nil, %{}} = DbStore.get(@conn, "unknown_sid", @opts)
   end
+
+  describe "signed" do
+    test "session data is signed" do
+      opts = @signing_opts.store_config
+
+      cookie = DbStore.put(@conn, nil, %{"foo" => "baz"}, opts)
+      assert is_binary(cookie)
+
+      assert DbStore.get(@conn, cookie, opts) ==
+               {cookie, %{"foo" => "baz"}}
+
+      assert DbStore.get(@conn, "bad", opts) == {nil, %{}}
+    end
+
+    test "gets and sets signed session data" do
+      conn =
+        build_conn(:get, "/")
+        |> sign_conn()
+        |> put_session("foo", "bar")
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> sign_conn()
+             |> get_session("foo") == "bar"
+    end
+
+    test "deletes session cookie" do
+      conn =
+        build_conn(:get, "/")
+        |> sign_conn()
+        |> put_session("foo", "bar")
+        |> configure_session(drop: true)
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> sign_conn()
+             |> get_session("foo") == nil
+    end
+  end
+
+  describe "encrypted" do
+    test "session data is encrypted" do
+      opts = @encrypted_opts.store_config
+      cookie = DbStore.put(@conn, nil, %{"foo" => "baz"}, opts)
+      assert is_binary(cookie)
+
+      assert DbStore.get(@conn, cookie, opts) ==
+               {cookie, %{"foo" => "baz"}}
+
+      assert DbStore.get(@conn, "bad", opts) == {nil, %{}}
+    end
+
+    test "gets and sets encrypted session data" do
+      conn =
+        build_conn(:get, "/")
+        |> encrypt_conn()
+        |> put_session("foo", "bar")
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> encrypt_conn()
+             |> get_session("foo") == "bar"
+    end
+
+    test "deletes session cookie" do
+      conn =
+        build_conn(:get, "/")
+        |> encrypt_conn()
+        |> put_session("foo", "bar")
+        |> configure_session(drop: true)
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> encrypt_conn()
+             |> get_session("foo") == nil
+    end
+  end
+
+  describe "custom serializer" do
+    test "session data is serialized by the custom serializer" do
+      opts = @custom_serializer_opts.store_config
+      cookie = DbStore.put(@conn, nil, %{"foo" => "baz"}, opts)
+      assert is_binary(cookie)
+
+      assert DbStore.get(@conn, cookie, opts) ==
+               {cookie, %{"foo" => "baz"}}
+    end
+
+    test "gets and sets custom serialized session data" do
+      conn =
+        build_conn(:get, "/")
+        |> custom_serialize_conn()
+        |> put_session("foo", "bar")
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> custom_serialize_conn()
+             |> get_session("foo") == "bar"
+    end
+
+    test "deletes session cookie" do
+      conn =
+        build_conn(:get, "/")
+        |> custom_serialize_conn()
+        |> put_session("foo", "bar")
+        |> configure_session(drop: true)
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> custom_serialize_conn()
+             |> get_session("foo") == nil
+    end
+  end
+
+  describe "prederivation" do
+    test "gets and sets prederived session data" do
+      conn =
+        build_conn(:get, "/")
+        |> prederived_conn()
+        |> put_session("foo", "bar")
+        |> send_resp(200, "")
+
+      assert build_conn(:get, "/")
+             |> recycle_cookies(conn)
+             |> prederived_conn()
+             |> get_session("foo") == "bar"
+    end
+  end
+
+  def returns_arg(arg), do: arg
+
+  defp sign_conn(conn, secret \\ @secret) do
+    put_in(conn.secret_key_base, secret)
+    |> Plug.Session.call(@signing_opts)
+    |> fetch_session
+  end
+
+  defp encrypt_conn(conn) do
+    put_in(conn.secret_key_base, @secret)
+    |> Plug.Session.call(@encrypted_opts)
+    |> fetch_session
+  end
+
+  defp prederived_conn(conn) do
+    put_in(conn.secret_key_base, @secret)
+    |> Plug.Session.call(@prederived_opts)
+    |> fetch_session
+  end
+
+  defp custom_serialize_conn(conn) do
+    put_in(conn.secret_key_base, @secret)
+    |> Plug.Session.call(@custom_serializer_opts)
+    |> fetch_session
+  end
+
+  defp apply_mfa({module, function, args}), do: apply(module, function, args)
 end
